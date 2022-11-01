00609ff285a274584724e757e0eedf66f915073a
Jason <leongjason3781@gmail.com>
2022-11-02 00:41:39 +0800

    Added session so that we can update the islogin column in sql server table customer

--- h.py
+++ h.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
 import pyodbc
 app = Flask(__name__,template_folder='templates')
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 def connection(query,getResult = True):
     s = 'DESKTOP-EGO85AB\SQLEXPRESS' #Your server name
@@ -25,11 +28,15 @@ def connection(query,getResult = True):
 @app.route("/", methods=['POST','GET'])
 def hello_world():
     # result = connection("insert into customer values ('jason', 'leong', 23, '123000@gmail.com', 'M')",getResult=False)
-    result = connection("select * from customer ", getResult=True)
+    if request.method == 'POST':
+        result = connection("select email from customer where id = 1", getResult=True)
+        session['email'] = result
+        return redirect(url_for('result'))
     return render_template('test.html')
 
 @app.route("/result")
 def result():
+    print(session['email'][0][0])
     return render_template('result.html')
 
 
