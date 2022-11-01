9e6742f91095def5a3e93d7449322a343fa31d32
Jason <leongjason3781@gmail.com>
2022-11-01 22:51:16 +0800

    Added the bcrypt code that we discussed earlier and also linked the video where I learned to use bcrypt for everyone reference

--- test.py
+++ test.py
@@ -11,3 +11,19 @@ from datetime import datetime
 # print(after-before)
 
 
+import bcrypt
+def Encrypt(p):
+
+    #Got the bcrypt idea from this youtube video https://www.youtube.com/watch?v=CSHx6eCkmv0
+    passwd = bytes(p,encoding='UTF-8')
+
+    salt = bcrypt.gensalt() # this generates a random string to be appended to the end of password and hashpw will hash both of these strings
+    hashed = bcrypt.hashpw(passwd, salt)
+    return hashed
+
+hashed = Encrypt('hello')
+
+if bcrypt.checkpw(b'hello', hashed): # checkpw will decrypt it and then return
+    print("match")
+else:
+    print("does not match")
