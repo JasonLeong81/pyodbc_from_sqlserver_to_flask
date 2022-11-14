bde10f40d4ed7b1be57a05f6e0084424e3724827
Jason <leongjason3781@gmail.com>
2022-11-14 18:41:20 +0800

    figured out how to get the images working

--- h.py
+++ h.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
 import pyodbc
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__,template_folder='templates',static_folder='templates/static')
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
@@ -25,14 +25,15 @@ def connection(query,getResult = True):
 
 
 
-@app.route("/hello", methods=['POST','GET'])
+@app.route("/", methods=['POST','GET'])
 def hello_world():
     # result = connection("insert into customer values ('jason', 'leong', 23, '123000@gmail.com', 'M')",getResult=False)
     if request.method == 'POST':
         result = connection("select email from customer where id = 1", getResult=True)
         session['email'] = result
         return redirect(url_for('result'))
-    return render_template('test.html',r = {'a':'a','b':'b'})
+    t = url_for('static', filename='images/AI.jpg')
+    return render_template('test.html',r = {'a':'a','b':'b','testing':t})
 
 @app.route("/result")
 def result():
